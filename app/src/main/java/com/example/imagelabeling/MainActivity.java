9ac0321b8ec199292c65669c86f1dad1dcc208e4
Jonathan Dite <jdite@student.tgm.ac.at>
2022-01-20 13:57:40 +0100

    work in progress2

--- app/src/main/java/com/example/imagelabeling/MainActivity.java
+++ app/src/main/java/com/example/imagelabeling/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.imagelabeling;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,9 +18,22 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.label.ImageLabel;
 import com.google.mlkit.vision.label.ImageLabeler;
@@ -25,18 +41,66 @@ import com.google.mlkit.vision.label.ImageLabeling;
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
     TextView t;
     ActivityResultLauncher<Intent> resLaunch;
     StringBuilder sb,c;
+    ImageCapture imageCapture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img = findViewById(R.id.img);
         t = findViewById(R.id.text);
+        setContentView(R.layout.activity_main);
+        Log.i("myTAG", "bbbbbbbbbbbbbbbbbbbbbbbbb: "+ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)+" "+ContextCompat.checkSelfPermission(this, Manifest.permission.BIND_TV_INPUT));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
+                    50); }
+        Log.i("myTAG", "aaaaaaaaaaaaaaaaaaaaaaaaa: "+ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA));
+        PreviewView previewView = findViewById(R.id.previewView);
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // Camera provider is now guaranteed to be available
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                // Set up the view finder use case to display camera preview
+                Preview preview = new Preview.Builder().build();
+
+                // Set up the capture use case to allow users to take photos
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .build();
+
+                // Choose the camera by requiring a lens facing
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .build();
+
+                // Attach use cases to the camera with the same lifecycle owner
+                Camera camera = cameraProvider.bindToLifecycle(
+                        ((LifecycleOwner) this),
+                        cameraSelector,
+                        preview,
+                        imageCapture);
+
+                // Connect the preview use case to the previewView
+                preview.setSurfaceProvider(
+                        previewView.getSurfaceProvider());
+            } catch (InterruptedException | ExecutionException e) {
+                // Currently no exceptions thrown. cameraProviderFuture.get()
+                // shouldn't block since the listener is being called, so no need to
+                // handle InterruptedException.
+            }
+        }, ContextCompat.getMainExecutor(this));
+
         resLaunch = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
@@ -45,24 +109,7 @@ public class MainActivity extends AppCompatActivity {
                         if (result.getResultCode() == Activity.RESULT_OK) {
                             Intent data = result.getData();
                             Bundle extras = data.getExtras();
-                            Bitmap bitmapOriginal = (Bitmap) extras.get("data");
-                            DisplayMetrics metrics = new DisplayMetrics();
-                            MainActivity.this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                            int width = bitmapOriginal.getWidth();
-                            int height = bitmapOriginal.getHeight();
-
-                            float scaleWidth = metrics.scaledDensity;
-                            float scaleHeight = metrics.scaledDensity;
-
-                            // create a matrix for the manipulation
-                            Matrix matrix = new Matrix();
-                            // resize the bit map
-                            matrix.postScale(scaleWidth, scaleHeight);
-
-                            // recreate the new Bitmap
-                            scaledBitmap = Bitmap.createBitmap(bitmapOriginal, 0, 0, width, height, matrix, true);
-                            img.setImageBitmap(Bitmap.createScaledBitmap(imageBitmap, 200, 1200, true));
-                            //img.setImageBitmap(imageBitmap);
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
                             ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
                             InputImage image = InputImage.fromBitmap(imageBitmap, 0);
                             labeler.process(image)
@@ -85,6 +132,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void takePic(View v) {
-        resLaunch.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+        //resLaunch.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+        Log.i("myTAG", "take pic");
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), new ImageCapture.OnImageCapturedCallback() {
+            @Override
+            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                Log.i("myTAG", "image captured");
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                super.onError(exception);
+                Log.i("myTAG", "image failed "+exception.toString());
+            }
+        });
     }
 }
