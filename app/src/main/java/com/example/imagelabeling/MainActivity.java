58dede0673e11fcc2eec97ac61a4a54ba3aa9ed9
jdite <jdite@student.tgm.ac.at>
2022-10-18 22:51:54 +0200

    Revert "work in progress"
    This reverts commit 4e5cfffaf4af09b3bad9e0cd426c5ece6473076f.

--- app/src/main/java/com/example/imagelabeling/MainActivity.java
+++ app/src/main/java/com/example/imagelabeling/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.imagelabeling;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     TextView t;
     ActivityResultLauncher<Intent> resLaunch;
     StringBuilder sb,c;
+    boolean firstTime = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,27 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
+                        if(firstTime) {
+                            findViewById(R.id.startText).setVisibility(View.INVISIBLE);
+                            firstTime = false;
+                        }
                         if (result.getResultCode() == Activity.RESULT_OK) {
                             Intent data = result.getData();
                             Bundle extras = data.getExtras();
-                            Bitmap bitmapOriginal = (Bitmap) extras.get("data");
-                            DisplayMetrics metrics = new DisplayMetrics();
-                            MainActivity.this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                            int width = bitmapOriginal.getWidth();
-                            int height = bitmapOriginal.getHeight();
-
-                            float scaleWidth = metrics.scaledDensity;
-                            float scaleHeight = metrics.scaledDensity;
-
-                            // create a matrix for the manipulation
-                            Matrix matrix = new Matrix();
-                            // resize the bit map
-                            matrix.postScale(scaleWidth, scaleHeight);
-
-                            // recreate the new Bitmap
-                            scaledBitmap = Bitmap.createBitmap(bitmapOriginal, 0, 0, width, height, matrix, true);
-                            img.setImageBitmap(Bitmap.createScaledBitmap(imageBitmap, 200, 1200, true));
-                            //img.setImageBitmap(imageBitmap);
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            img.setImageBitmap(imageBitmap);
                             ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
                             InputImage image = InputImage.fromBitmap(imageBitmap, 0);
                             labeler.process(image)
